a50ef2521c49647a4674586314a7242b8c580ed9
Mir Josimuddin <jasimmir.mj@gmail.com>
2026-08-02 07:05:36 +0530

    Test Fix

--- src/test/java/com/mir/payhub/PayhubBackendApplicationTests.java
+++ src/test/java/com/mir/payhub/PayhubBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.mir.payhub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PayhubBackendApplicationTests {
 
 	@Test
